f47520c24a7bfed70150143651fc17c5295d526a
Leonardo Nunes Decaris <leonardodecaris8040@gmail.com>
2025-02-24 22:28:06 -0300

    v0.01 Inicial

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import express from "express";
+import sequelize from "./config/database";
+import UserModel from "./models/userModel";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/users", async (req, res) => {
+  const users = await UserModel.findAll();
+  res.json(users);
+});
+
+// Sync Database
+sequelize
+  .sync({ alter: true })
+  .then(() => {
+    console.log("Database encontrado");
+  })
+  .catch((error) => {
+    console.log("Database não encontrado");
+  });
+
+app.listen(port, () => {
+  console.log("server is running on port", port);
+});

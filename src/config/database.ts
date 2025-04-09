863ffaa2f07158f66e7c622e7df37be9fd193331
Lucas Pedrozo <161781256+lucas-pedrozo@users.noreply.github.com>
2025-04-08 21:09:00 -0300

    correção no database alter

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,26 +1,26 @@
-import { Sequelize } from 'sequelize'
-const isTest = process.env.NODE_ENV === 'test'
+import { Sequelize } from "sequelize";
+const isTest = process.env.NODE_ENV === "test";
 
 const sequelize = new Sequelize(
-    isTest ? 'harmonicsound_test' : 'harmonicsound_homolog',
-    'root',
-    '',
-    {
-        host: 'localhost',
-        dialect: 'mysql',
-        logging: !isTest
-    }
+  isTest ? "harmonicsound_test" : "harmonicsound_homolog",
+  "root",
+  "",
+  {
+    host: "localhost",
+    dialect: "mysql",
+    logging: !isTest,
+  }
 );
 
-if (process.env.NODE_ENV !== 'test') {
+if (process.env.NODE_ENV !== "test") {
   (async () => {
     try {
-      await sequelize.sync({ alter: true });
-      console.log('Banco de dados sincronizado.');
+      await sequelize.sync();
+      console.log("Banco de dados sincronizado.");
     } catch (error) {
-      console.error('Erro ao sincronizar o banco de dados:', error);
+      console.error("Erro ao sincronizar o banco de dados:", error);
     }
   })();
 }
 
-export default sequelize
+export default sequelize;
